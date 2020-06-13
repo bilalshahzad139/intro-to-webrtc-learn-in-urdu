ee3d8f57d6787cea32393a30ae670c1d8a918e18
Bilal Shahzad <bilal.shahzad@ibexglobal.com>
2020-06-13 21:35:39 +0500

    .

--- SignalingServer/SignalingServer/Models/MyHub1.cs
+++ SignalingServer/SignalingServer/Models/MyHub1.cs
@@ -19,7 +19,7 @@ namespace WebApplication5.Models
     {
         public static List<UserConnectionData> _userConnections = new List<UserConnectionData>();
 
-        public void Connect(String user_id,String meetingid)
+        public Object Connect(String user_id,String meetingid)
         {
             var obj = _userConnections.Where(p => p.user_id == user_id && p.meeting_id == meetingid).FirstOrDefault();
             if(obj != null)
@@ -34,12 +34,19 @@ namespace WebApplication5.Models
                     connectionId = Context.ConnectionId
                 });
             }
-            var list = _userConnections.Where(p => p.meeting_id == meetingid).ToList();
-            var users = list.Select(p => p.user_id).ToList();
-            foreach (var v in list)
+            var other_user = _userConnections.Where(p => p.meeting_id == meetingid && p.user_id != user_id).FirstOrDefault();
+            if(other_user != null)
             {
-                Clients.Client(v.connectionId).informAboutUsers(users);
+                Clients.Client(other_user.connectionId).informAboutUsers(user_id);
+                return other_user;
             }
+
+            return null;
+            //var users = list.Select(p => p.user_id).ToList();
+            //foreach (var v in list)
+            //{
+            //    Clients.Client(v.connectionId).informAboutUsers(users);
+            //}
         }
         public override Task OnDisconnected(bool stopCalled)
         {
@@ -66,6 +73,21 @@ namespace WebApplication5.Models
             {
                 Clients.Client(v.connectionId).newMessage(message);
             }
+        }
+
+        public void userLeft()
+        {
+            var connectionId = Context.ConnectionId;
+            var meeting_id = _userConnections.Where(p => p.connectionId == connectionId).Select(p => p.meeting_id).FirstOrDefault();
+
+            _userConnections.RemoveAll(p => p.connectionId == connectionId);
+            var list = _userConnections.Where(p => p.meeting_id == meeting_id).ToList();
+            var users = list.Select(p => p.user_id).ToList();
+            foreach (var v in list)
+            {
+                Clients.Client(v.connectionId).otherUserLeft();
+            }
+
         }
     }
 }
